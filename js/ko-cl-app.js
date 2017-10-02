8883d6ed298edccfc7cc58e0bd99df4f69d5116f
Michael O'Sullivan <michael@nizamimedia.com>
2017-10-03 00:54:03 +0100

    Added in for loops for nicknames and levels

--- js/ko-cl-app.js
+++ js/ko-cl-app.js
@@ -1,19 +1,47 @@
-console.log("WORKING")
+
 
 var ViewModel = function() {
 
 	var catName = 'Tabby';
 
+	//LEVELS:
+
+	
+
 	this.clickCount = ko.observable(0); //0 is the default setting for this event listeners
 	this.name = ko.observable(catName);
 	this.imgSrc = ko.observable('https://i.pinimg.com/736x/2e/18/ab/2e18ab3f71b73c6719b04c81625bb922--cat-cute-cute-pets.jpg');
 	this.imgAttribution = ko.observable('https://i.pinimg.com/736x/2e/18/ab/2e18ab3f71b73c6719b04c81625bb922--cat-cute-cute-pets.jpg');
+	this.catLevel = ko.observable(0);
+	this.nickNames = ko.observableArray(['Momo', 'Lewis', 'Cookie', 'Fluffy']);
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 
-
 	};
+
+
+
+	this.catLevel = ko.computed(function(){
+
+		if (this.clickCount() <= 10){
+
+			$('#cat-level').html("Kitten");
+
+		} else if (this.clickCount() <= 20){
+
+			$('#cat-level').html("Young Cat");
+
+		} else {
+
+			$('#cat-level').html("Adult Cat");
+		}
+
+	}, this);
+
+	
+
+
 }
 
 ko.applyBindings(new ViewModel());
--- ko-js.php
+++ ko-js.php
@@ -10,13 +10,22 @@
 
 				    <ul id="cat-list"></ul>
 				    <div id="cat">
-
+				    	<h2 id="cat-level"></h2>
 				        <h2 data-bind="text: name" id="cat-name"></h2>
 				        <div data-bind="text: clickCount" id="cat-count"></div> <!-- text means the text you want to display -->
 				        <img data-bind="click: incrementCounter, attr: {src: imgSrc}" id="cat-img" src="" alt="cute cat" class="img-fluid" width="50%">
 
 				    </div>
 
+				    <div>
+				    	<h4>Nick Names Bitches</h4>
+					    <ul data-bind="foreach: nickNames">
+
+					    	<li data-bind="text: $data"></li>
+
+					    </ul>
+
+					</div>
 					
 			</div>
 
